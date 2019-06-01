403ed798bc9ac2967ac54345d115bfdce3aeb967
olivier levasseur <levasseuro.ol@gmail.com>
2019-06-01 14:39:22 -0400

    Lecture des lignes du fichier passe en parametre

--- mips.c
+++ mips.c
@@ -1,4 +1,53 @@
 #include <stdio.h>
-main() {
-    printf("Hello World\n");
+#include <stdlib.h>
+
+int main(int argc, char ** argv) {
+    if (argc <= 1) {
+        customExit("Error, no mips file was passed as an argument to be encoded");
+    }
+    FILE *mipsFilePointer = fopen(argv[1],"r");
+    if (mipsFilePointer == NULL) {
+        customExit("Error while opening file");    
+    }
+    char ch;
+    int lineLengthCounter = 0;
+    char * oneInstruction = (char*)calloc(32, sizeof(char));
+    while ((ch = fgetc(mipsFilePointer)) != EOF) {
+        //printf("%c", ch);
+        
+        if (ch == '\n') {
+            //printf("%s of length %d", "end of line!!!", lineLengthCounter);
+            oneInstruction[lineLengthCounter] = '\0';
+            lineLengthCounter = 0;
+            printLine(oneInstruction);
+        } else {
+            oneInstruction[lineLengthCounter] = (char) ch;
+            lineLengthCounter++;
+        }
+
+    }
+    fclose(mipsFilePointer);
+    return 0;
 }
+
+printLine(char * line) {
+   printf("%s\n",line); 
+}
+
+customExit(char * message) {
+    printf("%s\n", message);
+    exit(1);
+}
+
+/*public char ** getMipsInstructionsFromFile(char * filePath) {
+    //
+    FILE *fp;
+    fp = fopen(filePath, "r");
+    if (fp != NULL) {
+        
+    } else {
+        printf("Error while opening file\n");
+        exit(1);
+    }
+
+}*/
