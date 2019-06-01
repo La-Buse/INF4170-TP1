b0b86f75911a24d9f0435b156e08e162e0e06090
olivier levasseur <levasseuro.ol@gmail.com>
2019-06-01 15:27:50 -0400

    Ajout de fonction qui retourne valeur du registre selon son nom

--- mips.c
+++ mips.c
@@ -26,6 +26,8 @@ int main(int argc, char ** argv) {
         }
 
     }
+    printf("%s : %d", "result from register function with a$0", getRegisterNumberValue("$a0"));
+    //getRegisterNumberValue("$a0");
     fclose(mipsFilePointer);
     return 0;
 }
@@ -37,6 +39,43 @@ printLine(char * line) {
 customExit(char * message) {
     printf("%s\n", message);
     exit(1);
+}
+
+int getRegisterNumberValue(char *name) {
+        if (strcmp(name, "$zero") == 0) return 0;
+        else if (strcmp(name, "$at") == 0) return 1;
+        else if (strcmp(name, "$v0") == 0) return 2;
+        else if (strcmp(name, "$v1") == 0) return 3;
+        else if (strcmp(name, "$a0") == 0) return 4;
+        else if (strcmp(name, "$a1") == 0) return 5;
+        else if (strcmp(name, "$a2") == 0) return 6;
+        else if (strcmp(name, "$a3") == 0) return 7;
+        else if (strcmp(name, "$t0") == 0) return 8;
+        else if (strcmp(name, "$t1") == 0) return 9;
+        else if (strcmp(name, "$t2") == 0) return 10;
+        else if (strcmp(name, "$t2") == 0) return 11;
+        else if (strcmp(name, "$t3") == 0) return 12;
+        else if (strcmp(name, "$t4") == 0) return 13;
+        else if (strcmp(name, "$t5") == 0) return 14;
+        else if (strcmp(name, "$t6") == 0) return 15;
+        else if (strcmp(name, "$t7") == 0) return 16;
+        else if (strcmp(name, "$s0") == 0) return 17;
+        else if (strcmp(name, "$s1") == 0) return 18;
+        else if (strcmp(name, "$s2") == 0) return 19;
+        else if (strcmp(name, "$s3") == 0) return 20;
+        else if (strcmp(name, "$s4") == 0) return 21;
+        else if (strcmp(name, "$s5") == 0) return 22;
+        else if (strcmp(name, "$s6") == 0) return 23;
+        else if (strcmp(name, "$s7") == 0) return 24;
+        else if (strcmp(name, "$t8") == 0) return 25;
+        else if (strcmp(name, "$t9") == 0) return 26;
+        else if (strcmp(name, "$k0") == 0) return 27;
+        else if (strcmp(name, "$k1") == 0) return 28;
+        else if (strcmp(name, "$gp") == 0) return 29;
+        else if (strcmp(name, "$sp") == 0) return 30;
+        else if (strcmp(name, "$fp") == 0) return 31;
+        else if (strcmp(name, "$ra") == 0) return 32;
+
 }
 
 /*public char ** getMipsInstructionsFromFile(char * filePath) {
